bfd44dd99863d61caef3f3c716460634df12b8c2
Jonathan van Rij <jivanrij@gmail.com>
2015-10-17 21:28:05 +0200

    wrote all the css information into the less files

--- web/sites/all/themes/gojiratheme/page.tpl.php
+++ web/sites/all/themes/gojiratheme/page.tpl.php
@@ -1,6 +1,5 @@
 <?php
 $overlay = '<div id="overlay_wait" class="overlay_wait"></div><div id="overlay_wait_txt" class="overlay_wait"></div>';
-$sLowResWarning = '<div id="low_res_warning">'.t('SocialeKaart.care is not developed for small mobile devices. Please return to the site with a tablet of PC or find another way to raise your resolution.').'</div>';
 switch(Template::getView()){
   case Template::VIEWTYPE_AJAX:
     print render($page['content']);
