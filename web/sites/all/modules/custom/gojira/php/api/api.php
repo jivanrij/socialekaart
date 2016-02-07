2abd4e2e03e28b4c4d0ffe2c6a2d77fb81cfe380
Jonathan van Rij <jivanrij@gmail.com>
2016-02-07 22:46:51 +0100

    Adding some headers to the api rest test page

--- web/sites/all/modules/custom/gojira/php/api/api.php
+++ web/sites/all/modules/custom/gojira/php/api/api.php
@@ -4,13 +4,19 @@
  */
 function api_locations() {
     
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Allow-Origin:*');
+header('Cache-Control:no-cache');
+header('Connection:keep-alive');
+header('Content-Length:0');
+
   $values = array();
   
   $locations = db_query("select nid from node where type = 'location' limit 10");
   
   foreach($locations as $location){
       $node = node_load($location->nid);
-      $values[$location->nid] = array('title'=>$node->title);
+      $values[$location->nid] = array('title'=>$node->title, 'adres'=>Location::getAddressString($node), 'labels'=>Labels::getLabels($node));
   }
   
   echo json_encode($values);
