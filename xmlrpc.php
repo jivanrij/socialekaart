6960a3bff8e8bfc566118b92f152b89d7a23b25f
Jonathan van Rij <jivanrij@gmail.com>
2018-04-13 12:09:34 +0200

    Adds nice formatting to the code

--- xmlrpc.php
+++ xmlrpc.php
@@ -14,5 +14,4 @@ include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 include_once DRUPAL_ROOT . '/includes/xmlrpc.inc';
 include_once DRUPAL_ROOT . '/includes/xmlrpcs.inc';
-
 xmlrpc_server(module_invoke_all('xmlrpc'));
